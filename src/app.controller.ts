d932f0fe30ffe3463d78c06064a1b1f960c09152
Grimmer <grimmer0125@gmail.com>
2022-11-21 14:29:27 +0800

    Add sample DTO files to test npm run gen-json-schema

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,9 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { AppService } from './app.service';
+
+import { SumDto } from './dto/sum.dto';
+
+import { SubtractDto } from './dto/subtract.dto';
 
 @Controller()
 export class AppController {
@@ -9,4 +13,26 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Post('sum')
+  async testSum(@Body() SumDto: SumDto) {
+    const { numbers } = SumDto;
+    const initialValue = 0;
+    const sumWithInitial = numbers.reduce(
+      (accumulator, currentValue) => accumulator + currentValue,
+      initialValue,
+    );
+    return sumWithInitial;
+  }
+
+  // @Post('subtract')
+  // async testSubtract(@Body() SumDto: SubtractDto) {
+  //   const { numberArray } = SumDto;
+  //   const initialValue = 0;
+  //   const sumWithInitial = numberArray.reduce(
+  //     (accumulator, currentValue) => accumulator - currentValue,
+  //     initialValue,
+  //   );
+  //   return sumWithInitial;
+  // }
 }
